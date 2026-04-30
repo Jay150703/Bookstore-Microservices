75e0c3a6011708f894953cc3c066a0b7dfa0cc5d
Jay150703 <jayanthijemi15@gmail.com>
2026-04-30 11:37:00 +0530

    [Jayanthi] : Admin service only

--- product-service/src/main/java/com/bookstore/product/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.bookstore.product.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- wishlist--service/src/main/java/com/bookstore/wishlist/repository/WishListRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bookstore.wishlist.repository;
-
-import com.bookstore.wishlist.entity.WishList;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface WishListRepository extends JpaRepository<WishList, Long> {
-    Optional<WishList> findByUserId(String userId);
-}
